f6b4212ebe0c3af7ab7bc13150af9711adb8910a
fedepoi <federico.vivaldo@gmail.com>
2020-03-18 23:06:28 +0000

    new project

--- netbeansProject/Game2/src/game2/FXMLDocumentController.java
+++ netbeansProject/Game2/src/game2/FXMLDocumentController.java
@@ -5,7 +5,8 @@
  */
 package game2;
 
-import com.jfoenix.controls.*;
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
@@ -20,9 +21,20 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.layout.TilePane;
+
 
 /**
  *
@@ -41,20 +53,33 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private Button RegisterBeginning;
-
     @FXML
     private Button SignInBeginning;
-    
-    @FXML 
-    AnchorPane page;
-    
+    @FXML
+    private AnchorPane page;
+    @FXML
+    private HBox MenuBar;
+    @FXML
+    private Button SettingsMenu;
+    @FXML
+    private Button ReportsMenu;
+    @FXML
+    private Button LogOut;
+    @FXML
+    private Button MathsImage;
+    @FXML
+    private Button answerMaths1;
+    @FXML
+    private Button answerMaths2;
+    @FXML
+    private Button answerMths3;
+    @FXML
+    private Button answerMths4;
+    @FXML
+    private Button nextQuestion;
+    @FXML
+    private Button cancelQuestion;
      @FXML
-    private VBox RegisterBox;
-
-    @FXML
-    private Label RegisterLabel;
-
-    @FXML
     private JFXTextField FirstNameTextReg;
 
     @FXML
@@ -65,23 +90,14 @@ public class FXMLDocumentController implements Initializable {
 
     @FXML
     private JFXTextField PasswordTextReg;
-
-    @FXML
-    private Button RegisterButton;
-    @FXML
-    private VBox SignInBox;
-
-    @FXML
-    private Label SingInLabel;
-
-    @FXML
+      @FXML
     private JFXTextField EmailTextSignIn;
 
     @FXML
     private JFXTextField PasswordTextSignIn;
 
-    @FXML
-    private Button SignIn;
+    
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -133,11 +149,91 @@ public class FXMLDocumentController implements Initializable {
         });
     }
     
+   @FXML
+    private void openHome(ActionEvent event) throws SQLException{
+         try {
+           Parent windowHome = FXMLLoader.load(getClass().getResource("Home.fxml"));
+           Scene windowHomeScene = new Scene(windowHome);
+           Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+           window.setScene(windowHomeScene);
+           window.show();
+       } catch (IOException ex) {
+           Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+       }    
+         signIn();
+               
+    }
+    
+   @FXML
+    private void Logout(ActionEvent event){
+         try {
+           Parent windowHome = FXMLLoader.load(getClass().getResource("RegisterSignIn.fxml"));
+           Scene windowHomeScene = new Scene(windowHome);
+           Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+           window.setScene(windowHomeScene);
+           window.show();
+       } catch (IOException ex) {
+           Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+       }    
+               
+    }
+    
+    @FXML
+    private void openMaths(ActionEvent event){
+         try {
+           Parent windowHome = FXMLLoader.load(getClass().getResource("QuizMaths.fxml"));
+           Scene windowHomeScene = new Scene(windowHome);
+           Stage window = (Stage)((Node)event.getSource()).getScene().getWindow();
+           window.setScene(windowHomeScene);
+           window.show();
+       } catch (IOException ex) {
+           Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+       }    
+               
+    }
+    @FXML
+    private void openAlert(ActionEvent event){
+        
+        //MISSING THE CONNECTION!WHERE IS MY WINDOW TO PUT THE ALERT BOX?
+        //I TRIES TO USE THE CODE FROM OPENMATHS BUT DIDNT WORK
+      
+         
+        TilePane r = new TilePane(); 
+  
+        // create a alert 
+        Alert a = new Alert(AlertType.NONE); 
+  
+        // action event 
+        EventHandler<ActionEvent> e = new EventHandler<ActionEvent>(){
+            public void handle(ActionEvent e) 
+            { 
+            // set alert type
+            a.setAlertType(AlertType.CONFIRMATION);
+            
+             a.setContentText("Do you want to exit from the quiz?");
+            
+            // show the dialog
+            a.show();
+            }
+          }; 
+              
+            
+            cancelQuestion.setOnAction(e);
+            
+            r.getChildren().add(cancelQuestion);
+            
+            
+  
+     
+          
+    }
+    
     @FXML
     public void register() throws SQLException
     {
     DBConnect connection= new DBConnect();
     connection.registerSQL(FirstNameTextReg.getText(), LastNameTextReg.getText(), EmailTextReg.getText(), PasswordTextReg.getText());
+       
     }
     @FXML
     public void signIn() throws SQLException
@@ -148,27 +244,5 @@ public class FXMLDocumentController implements Initializable {
   
     
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
