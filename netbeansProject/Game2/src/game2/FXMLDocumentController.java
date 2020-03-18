e2ce7bc8502492045fa0da61b0b8bfc5b8ed387e
fedepoi <federico.vivaldo@gmail.com>
2020-03-18 21:27:13 +0000

    ive done javafx

--- netbeansProject/Game2/src/game2/FXMLDocumentController.java
+++ netbeansProject/Game2/src/game2/FXMLDocumentController.java
@@ -145,7 +145,7 @@ public class FXMLDocumentController implements Initializable {
     DBConnect connection = new DBConnect();
     connection.signInSQL(EmailTextSignIn.getText(), PasswordTextSignIn.getText());
     
-    
+  
     
     }
     
